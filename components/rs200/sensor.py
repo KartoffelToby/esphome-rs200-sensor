1060baf10a19564cc3668a88a572aa80ea259e63
Tobias Haber <kontakt@t-haber.de>
2025-08-17 19:08:41 +0000

    [INIT]

--- components/rs200/sensor.py
+++ components/rs200/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import CONF_ID, UNIT_EMPTY, ICON_WATER, DEVICE_CLASS_EMPTY
+from esphome.const import CONF_ID, UNIT_EMPTY, ICON_WATER, DEVICE_CLASS_EMPTY, CONF_UART_ID
 
 DEPENDENCIES = ["uart"]
 
@@ -15,10 +15,12 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     device_class=DEVICE_CLASS_EMPTY,
 ).extend({
     cv.GenerateID(): cv.declare_id(RS200Sensor),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
 }).extend(cv.polling_component_schema("5s"))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], uart.UARTComponent.PARENT)
+    uart_var = await cg.get_variable(config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], uart_var)
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
     await sensor.register_sensor(var, config)
